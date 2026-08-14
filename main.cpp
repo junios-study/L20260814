8906f11638a392dbfb5f57cfdfe03e3c7f4b9b1e
kang98kang <123940888+JinsungB@users.noreply.github.com>
2026-08-14 11:50:26 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,55 +2,87 @@
 
 using namespace std;
 
+
+int Map[10][10] = {
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+//Global Variable
+int PlayerX = 1;
+int PlayerY = 1;
+
+
+//정의, 실행 X
+void Render()
+{
+	system("cls");
+
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << "P";
+			}
+			else if (Map[Y][X] == 1)
+			{
+				cout << "*";
+			}
+			else if (Map[Y][X] == 0)
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void Tick()
+{
+	PlayerX++;
+}
+
+void Input()
+{
+	int a;
+	cin >> a;
+}
+
+void PrintHelloWorld(int A, int B)
+{
+	return;
+
+	if (1)
+	{
+
+	}
+}
+
+
 int main()
 {
-	int PlayerX = 1;
-	int PlayerY = 1;
-
-	int Map[10][10] = {
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	};
-
-	//논리연산자
-	//A 그리고 B -> And -> && -> A && B
-	//A 거나 B -> Or -> || -> A || B
-	//A가 아님 -> Not -> !A
+	//절차대로 씀
+	//절차지향 프로그래밍
+	//함수로 만듬, 영어 동사
 	for (;;)
 	{
-		int a;
-		cin >> a;
-		system("cls");
-		PlayerX++;
-		for (int Y = 0; Y < 10; ++Y)
-		{
-			for (int X = 0; X < 10; ++X)
-			{
-				if (PlayerX == X && PlayerY == Y)
-				{
-					cout << "P";
-				}
-				else if (Map[Y][X] == 1)
-				{
-					cout << "*";
-				}
-				else if (Map[Y][X] == 0)
-				{
-					cout << " ";
-				}
-			}
-			cout << endl;
-		}
+		Input();
+		Tick();
+		Render();
 	}
 
 
 	return 0;
 }
+
+

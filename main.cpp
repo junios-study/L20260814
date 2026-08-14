5e97f90bd8d99808c43db1908d35cf7b510c34fc
kang98kang <123940888+JinsungB@users.noreply.github.com>
2026-08-14 15:57:24 +0900

    포인터

--- main.cpp
+++ main.cpp
@@ -17,9 +17,17 @@ int main()
 	cout << *PA << endl;
 	*PA = 2;
 
+
 	cout << A << endl;
 
+	//int*형 변수의 위치를 기억하는 자료형
+	int** PPA = nullptr;  
 
+	PPA = &PA;
+	**PPA = 10;
+	cout << A << endl;
+
+	int*** PPA = &PPA;
 
 	return 0;
 }

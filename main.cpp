7628f4fb9029eeafb24e0e48c479fcb33fb436e5
kang98kang <123940888+JinsungB@users.noreply.github.com>
2026-08-14 10:44:15 +0900

    논리연산자

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
+	int PlayerX = 1;
+	int PlayerY = 1;
+
 	int Map[10][10] = {
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -17,11 +20,20 @@ int main()
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
 
+	//논리연산자
+	//A 그리고 B -> And -> && -> A && B
+	//A 거나 B -> Or -> || -> A || B
+	//A가 아님 -> Not -> !A
+
 	for (int Y = 0; Y < 10; ++Y)
 	{
 		for (int X = 0; X < 10; ++X)
 		{
-			if (Map[Y][X] == 1)
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << "P";
+			}
+			else if (Map[Y][X] == 1)
 			{
 				cout << "*";
 			}

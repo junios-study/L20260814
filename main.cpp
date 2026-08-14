b6d861f0741aba13c50f543fb816073e771e17d4
kang98kang <123940888+JinsungB@users.noreply.github.com>
2026-08-14 15:44:01 +0900

    github 기본

--- main.cpp
+++ main.cpp
@@ -2,115 +2,22 @@
 
 using namespace std;
 
-
-
-//int Map[10][10] = {
-//	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-//};
-//
-////Global Variable
-//int PlayerX = 1;
-//int PlayerY = 1;
-//
-//
-////정의, 실행 X
-//void Render()
-//{
-//	system("cls");
-//
-//	for (int Y = 0; Y < 10; ++Y)
-//	{
-//		for (int X = 0; X < 10; ++X)
-//		{
-//			if (PlayerX == X && PlayerY == Y)
-//			{
-//				cout << "P";
-//			}
-//			else if (Map[Y][X] == 1)
-//			{
-//				cout << "*";
-//			}
-//			else if (Map[Y][X] == 0)
-//			{
-//				cout << " ";
-//			}
-//		}
-//		cout << endl;
-//	}
-//}
-//
-//void Tick()
-//{
-//	PlayerX++;
-//}
-//
-//void Input()
-//{
-//	int a;
-//	cin >> a;
-//}
-//
-//void PrintHelloWorld(int A, int B)
-//{
-//	return;
-//
-//}
-//
-////main.exe helloworld 1 2
-//
-////c언어? 만든 이유, OS 만들려고(unix)
-//int main()
-//{
-//	//절차대로 씀
-//	//절차지향 프로그래밍
-//	//함수로 만듬, 영어 동사
-//	for (;;)
-//	{
-//		Input();
-//		Tick();
-//		Render();
-//	}
-//
-//
-//	return 0;
-//}
-//
-//
-
-void Swap(int A, int B)
-{
-	int Temp = A;
-	A = B;
-	B = Temp;
-
-	cout << &A << endl;
-	cout << &B << endl;
-}
-
-
 int main()
 {
-	int A = 10;
-	int B = 20;
+	int A = 1;
+	//int형 변수의 주소를 기억하는 자료형
+	//int형 pointer 변수
+	int* PA = nullptr; //가르치는게 없음.
 
-	cout << &A << endl;
-	cout << &B << endl;
-	cout << "-------------------------------------------" << endl;
+	PA = &A;
 
-	Swap(&A, &B);
+	cout << &A << endl; //-> A의 주소(메모리 위치)
+	cout << PA << endl; 
 
-	cout << "-------------------------------------------" << endl;
+	cout << *PA << endl;
+	*PA = 2;
+
 	cout << A << endl;
-	cout << B << endl;
 
 
 
